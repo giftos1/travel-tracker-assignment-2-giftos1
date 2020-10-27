c73d8512f3438459cfbbef44d62121fd17c1abb2
Gift Ogingo <giftsydney676@gmail.com>
2020-10-27 16:37:28 +1000

    "Test place if is visited or not"

--- test_place.py
+++ test_place.py
@@ -6,17 +6,23 @@ def run_tests():
     """Test Place class."""
 
     # Test empty place (defaults)
-    print("Test empty place:")
+    """print("Test empty place:")
     default_place = Place()
     print(default_place)
     assert default_place.name == ""
     assert default_place.country == ""
     assert default_place.priority == 0
-    assert not default_place.is_visited
+    assert not default_place.is_visited"""
 
     # Test initial-value place
     print("Test initial-value place:")
     new_place = Place("Malagar", "Spain", 1, False)
+    print(new_place)
+
+    if new_place.is_unvisited():
+        print("This place is unvisited")
+    elif new_place.is_visited():
+        print("This place is visited")
     # TODO: Write tests to show this initialisation works
 
     # TODO: Add more tests, as appropriate, for each method
