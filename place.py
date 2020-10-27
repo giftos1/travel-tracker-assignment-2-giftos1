ed06725a58f72ae322af7f2b425d40098fe6a5b6
Gift Ogingo <giftsydney676@gmail.com>
2020-10-27 16:30:19 +1000

    "Create Place class with methods and docstrings for each method"

--- place.py
+++ place.py
@@ -1,9 +1,32 @@
-"""..."""
+"""Assignment 2 Place class"""
 
 
 # Create your Place class in this file
+IMPORTANT_PLACE = 2
 
 
 class Place:
-    """..."""
+    """Place class for storing details of a place"""
+    def __init__(self, name, country, priority, visited_status):
+        """Initialise a place."""
+        self.name = name
+        self.country = country
+        self.priority = priority
+        self.visited_status = visited_status
+
+    def __str__(self):
+        """Return a string representation of a place"""
+        return "{} in {} priority {}".format(self.name, self.country, self.priority)
+
+    def is_visited(self):
+        """Determine if place is visited"""
+        return self.visited_status == "v"
+
+    def is_unvisited(self):
+        """Determine if place is not visited"""
+        return self.visited_status == "n"
+
+    def is_important(self):
+        """Determine if a place is considered important"""
+        return self.priority <= IMPORTANT_PLACE
     pass
