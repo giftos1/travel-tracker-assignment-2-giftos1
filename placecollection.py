dc23e8f49ae31b317ef1b73497a6f2cbebbd6a59
giftos1 <giftsydney676@gmail.com>
2020-10-31 18:31:00 +1000

    "Load places from csv file"

--- placecollection.py
+++ placecollection.py
@@ -3,7 +3,26 @@
 
 # Create your PlaceCollection class in this file
 
+import csv
+from place import Place
+
 
 class PlaceCollection:
-    """..."""
+    """PlaceCollection class for loading files, saving files,sorting and adding Place objects"""
+    def __init__(self):
+        self.places = []
+
+    def load_places(self, places_file):
+        places_file = open(places_file)
+        places_reader = csv.reader(places_file)
+        for line in places_reader:
+            place_object = Place(line[0], line[1], line[2], line[3])
+            self.places.append(place_object)
+        return self.places
+
+
+
+
+
+
     pass
