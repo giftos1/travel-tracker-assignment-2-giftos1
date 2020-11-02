44e3cd201b04b65c2869ee4c144bb2fe3c02be04
Gift <giftsydney676@gmail.com>
2020-11-02 22:50:34 +1000

    "Append new place object to objects in list"

--- a1_classes.py
+++ a1_classes.py
@@ -61,3 +61,47 @@ def display_places(places):
         else:
             print(" " + str(places_count), "{}".format(
                 elements))  # prints data without asterix for list containing string v
+
+def list_places(places):
+    """Call display_places function and print an additional line containing the number count of places"""
+    visit_status_count = 0
+    place_count = 0
+    for i, place in enumerate(places):
+        place_count += 1
+        if place.is_unvisited:  # checks if "n" is in the places list indexes and prints the respective output.
+            visit_status_count += 1
+    display_places(places)
+
+    print("{} places. You still want to visit ".format(place_count), visit_status_count,
+                  "places.")  # Output to be printed if "n" is in the places list indexes
+
+    #print(place_count, "places. No places left to visit. Why not add a new place?")  # Output to be printed if "n" is not in the place object
+
+
+def add_new_place(places):
+    """Get user input and check for blank and ValueErrors. Store inputs in a list and append to the lists of lists(places_data)"""
+    name = input("Name:")  # Gets name of the town/city user has not visited
+    while name == "":
+        print("Input can not be blank")
+        name = input("Name:")
+
+    country = input("Country:")  # Gets name of the country user has not visited
+    while country == "":
+        print("Input can not be blank")
+        country = input("Country:")
+    priority = False
+
+    while not priority:  # Checks for ValueError for the priority input which should be an integer
+        try:
+            priority = int(input("Priority:"))  # Gets the digit of priority the user priorites the place not visited.
+            while priority <= 0:
+                print("Number must be > 0")
+                priority = int(input("Priority:"))
+            print(name, "in", country, "(priority", priority,
+                  ") added to Travel Tracker")  # Displays inputs given(name, country, priority)
+
+        except ValueError:
+            print("Invalid input; enter a valid number")
+    new_place_object = Place(name, country, priority, "n") # Stores inputs in a list with added place being put as not visited "n"
+    places.append(new_place_object)  # adds inputs list to the original list of lists(places_data) in main()
+    places.sort(key=attrgetter("visited_status", "priority"))  # sorts data appended with new_object that was originally in the list in priority given
