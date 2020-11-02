9e0d8e8d92338e7c284e7681418566693f683114
giftos1 <giftsydney676@gmail.com>
2020-11-02 18:57:31 +1000

    "Add sort, add_place and count_unvisited_places count"

--- placecollection.py
+++ placecollection.py
@@ -5,6 +5,7 @@
 
 import csv
 from place import Place
+from operator import attrgetter
 
 
 class PlaceCollection:
@@ -16,13 +17,21 @@ class PlaceCollection:
         places_file = open(places_file)
         places_reader = csv.reader(places_file)
         for line in places_reader:
+            line[2] = int(line[2])
             place_object = Place(line[0], line[1], line[2], line[3])
             self.places.append(place_object)
         return self.places
 
+    def sort(self, key):
+        return self.places.sort(key=attrgetter(key, "priority"))
 
+    def add_place(self, new_place):
+        return self.places.append(new_place)
 
-
-
-
+    def count_unvisited_places(self):
+        place_count = 0
+        for place in self.places:
+            if place.is_unvisited():
+                place_count += 1
+                return place_count
     pass
